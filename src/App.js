4e2676fb7287ac47d418e421ac70ab615568a7fa
JihenHs <jihen.hasnaoui1996@gmail.com>
2022-06-12 22:23:42 +0100

    adding jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import imageInSrc from "./imageInSrc.jpg";
+import "./style.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }} />
+
+        <h1 className="titleRed">Hasnaoui Jihen</h1>
+
+        <br />
+
+        <img
+          src="/imageInPublic.jpg"
+          alt="myImagepublic"
+          width="560"
+          height="315"
+        />
+
+        <br />
+
+        <img src={imageInSrc} alt="myImagesrc" width="560" height="315" />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/TmP1ohts41w"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </header>
     </div>
   );
